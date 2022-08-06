4481e9a5950fa7b38d15c38bb46b6b104fa2fd51
yang jiaolong <xiaoyi_yyc@163.com>
2022-08-06 16:53:35 +0800

    :poop: 跨域&登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -2,9 +2,9 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    // url: '/vue-admin-template/user/login',
-    // method: 'post',
-    // data
+    url: '/sys/login',
+    method: 'post',
+    data
   })
 }
 
